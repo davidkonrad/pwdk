2367beb0acf7aa64e93dbbab2b8d9f96390782bd
davidkonrad <davidkonrad@gmail.com>
2023-09-30 18:37:07 +0200

    refactor pw footer

--- templates/weidner/index.php
+++ templates/weidner/index.php
@@ -11,32 +11,29 @@ include('pages/header.php');
 	<div class="py-1">
 		<div class="container">
 			<div class="row d-block ml-1 mr-1">
-
 <?php
 $this->body();
 ?>
-
 			</div>
 		</div>
 	</div>
-
 </main>
 
+<?php
+if (!$this->hasProp('no-pw-footer')) {
+echo <<<FOOT
 <footer class="text-muted mt-sm-4">
 	<div class="container">
-		<p class="float-right"></p>
-			<div class="service-item ">
-						<div class="service-image icon-image">
-							<img style="height:100px;" src="assets/pernille-weidner-mugshot.jpg" class="img-thumbnail float-left mr-sm-3" alt="Pernille Weidner - psykoterapeut">
-						</div>
-						<div class="service-content">
-							<h4 class="service-title">Pernille Weidner</h4>
-							<p>Jeg er født i 1980 og bor i Taastrup med min kæreste og min hund.Jeg er uddannet pædagog, og har tidligere bl.a. arbejdet på en døgninstitution for unge med forskellige …</p>
-					</div>
-				</div>
-
-      </div>
-    </footer>
+		<img style="height:100px;" src="assets/pernille-weidner-mugshot.jpg" class="img-thumbnail float-left mr-sm-3" alt="Pernille Weidner - psykoterapeut">
+		<p class="pt-sm-1">
+			Jeg er uddannet pædagog, og har tidligere bl.a. arbejdet med ungediagnoser. I 2020 blev jeg uddannet psykoterapeut fra Københavns Gestalt Institut. Derudover har jeg taget forskellige kurser igennem årene som bl.a. mindfullness instruktør, positiv psykologi vejleder, ernæringskonsulent, fitnessinstruktør og som det nyeste, klangmassage behandler og klangaktør hos Anne Viese.
+			<a href="om-pernille-weidner" class="text-dark font-italic" title="Læs mere om Pernille Weidner">læs mere ...</a>
+		</p>
+	</div>
+</footer>
+FOOT;
+}
+?>
 
 <?php
 include('pages/footer.html');
