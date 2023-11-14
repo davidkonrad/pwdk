1cdc8ad5e6efacca49c8d537ef6a644d7161c48e
davidkonrad <davidkonrad@gmail.com>
2023-11-14 02:38:08 +0100

    Tilføjet link til 'Hjem'

--- pages/weidner-front.php
+++ pages/weidner-front.php
@@ -34,7 +34,7 @@
 		<div class="collapse navbar-collapse d-flex justify-content-center" id="navbarNavDropdown">
 			<ul class="navbar-nav">
 				<li class="nav-item active">
-					<a class="nav-link" href="#">Hjem <span class="sr-only">Hjem</span></a>
+					<a class="nav-link" href="https://www.pernilleweidner.dk">Hjem <span class="sr-only">Hjem</span></a>
 				</li>
 				<li class="nav-item">
 					<a class="nav-link" href="psykoterapi">Psykoterapi</a>
