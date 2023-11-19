ddd1615c3bda4b7613fd5c3e2247e37afd93c62e
davidkonrad <davidkonrad@gmail.com>
2023-11-19 15:18:10 +0100

    fix problem med indgående links som er beriget med tracking

--- pages/weidner-front.php
+++ pages/weidner-front.php
@@ -73,25 +73,31 @@ if (count($_GET)) {
 	$pages = array('klangmassage', 'kakao-ceremoni', 'psykoterapi', 'klangmeditation', 'gong-terapi', 'gong-bad',
 								'lydhealing', 'lydhealing-til-boern', 'lydhealing-stress-og-angst', 'kontakt');
 
-	$input = $_GET['page'];
-	$best = -1;
-	$suggest = '';
+	$input = strtolower($_SERVER['REQUEST_URI']);
+	$cinput = explode('?', $input)[0];
+	$cinput = str_replace('/', '', $cinput);
 
-	foreach ($pages as $word) {
-		$s = similar_text($word, $input);
-		if ($s > $best) {
-			$best = $s;
-			$suggest = $word;
-		}
+	if ($cinput !== $input) {
+		echo '<script>window.location.href = "https://www.pernilleweidner.dk/'.$cinput.'";</script>';
 	}
-
-	$recommend = $suggest !== '' ? 'Måske mente du <a href="'.$suggest.'" class="text-weidner">/'.$suggest.'</a>?' : '';
-
-	echo <<<HTML
-	<div class="alert alert-danger p-4 mr-4 ml-4">
-		<h5 class="albert">Desværre, siden <em class="text-muted">/{$_GET['page']}</em> findes ikke. {$recommend}</h5>
-	</div>
+	
+	if (!in_array($cinput, $pages)) {
+		$best = -1;
+		$suggest = '';
+		foreach ($pages as $word) {
+			$s = similar_text($word, $input);
+			if ($s > $best) {
+				$best = $s;
+				$suggest = $word;
+			}
+		}
+		$recommend = $suggest !== '' ? 'Måske mente du <a href="'.$suggest.'" class="text-weidner">/'.$suggest.'</a>?' : '';
+		echo <<<HTML
+		<div class="alert alert-danger p-4 mr-4 ml-4">
+			<h5 class="albert">Desværre, siden <em class="text-muted">/{$_GET['page']}</em> findes ikke. {$recommend}</h5>
+		</div>
 HTML;
+	}
 }
 ?>
 
